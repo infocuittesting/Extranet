45cdb3bdcab4519deacd33938195e0b2e2435992
iottech <iottech>
2018-09-21 13:09:23 +0530

    report

--- src/app/reports/reports.component.ts
+++ src/app/reports/reports.component.ts
@@ -25,7 +25,41 @@ export class ReportsComponent implements OnInit {
   public cancelcount=[];
   public modifycount;
   ngOnInit() {
-   
+    this.ReportsService.yearreservation()
+    .subscribe((resp: any) => {
+      // if (resp.ServiceStatus == 'Success') {
+        this.getroomdetails = resp.Returnvalue;
+        // this.cancelcount = resp.cancelcount;
+        // this.modifycount = resp.Totalbookingcount;
+        console.log("pie chrt work",this.getroomdetails);
+        this.chartDatas=[];
+        for(var i=0;i<this.getroomdetails.length;i++){
+          this.chartDatas.push({
+             'title':this.getroomdetails[i].title,
+             'value':this.getroomdetails[i].value })
+        }
+        // this.getroomdetails=[];
+          // console.log("$$$$$$",this.chartDatas);
+        this.chart = this.AmCharts.makeChart('chartdiv7', {
+          'type': 'pie',
+          'theme': 'light',
+          'hideCredits':true,
+          'dataProvider':this.chartDatas,
+          'titleField': 'title',
+          'valueField': 'value',
+          'labelRadius': 5,
+
+          'radius': '42%',
+          'innerRadius': '60%',
+          'labelText': '[[title]]',
+          'export': {
+            "legend": {
+              "display":"Users"
+            }
+          }
+        });
+        
+      });
 }
 fetchrecord(start_date,end_date){
   console.log("its coming to chart")
@@ -226,6 +260,41 @@ fetchrecord(start_date,end_date){
         // this.getroomdetails=[];
           // console.log("$$$$$$",this.chartDatas);
           this.chart = this.AmCharts.makeChart('chartdiv5', {
+            'type': 'pie',
+            'theme': 'light',
+            'hideCredits':true,
+            'dataProvider':this.chartDatas,
+            'titleField': 'title',
+            'valueField': 'value',
+            'labelRadius': 5,
+  
+            'radius': '42%',
+            'innerRadius': '0%',
+            'labelText': '[[title]]',
+            'export': {
+              "legend": {
+                "display":"Users"
+              }
+            }
+          });
+        
+      });
+      //country reservation
+      this.ReportsService.countryreservation(statsParms)
+      .subscribe((resp: any) => {   
+        this.getroomdetails = resp.Returnvalue;
+        // this.cancelcount = resp.cancelcount;
+        // this.modifycount = resp.Totalbookingcount;
+        console.log("pie chrt work",this.getroomdetails);
+        this.chartDatas=[];
+        for(var i=0;i<this.getroomdetails.length;i++){
+          this.chartDatas.push({
+             'title':this.getroomdetails[i].title,
+             'value':this.getroomdetails[i].value })
+        }
+        // this.getroomdetails=[];
+          // console.log("$$$$$$",this.chartDatas);
+          this.chart = this.AmCharts.makeChart('chartdiv6', {
             'type': 'pie',
             'theme': 'light',
             'hideCredits':true,
